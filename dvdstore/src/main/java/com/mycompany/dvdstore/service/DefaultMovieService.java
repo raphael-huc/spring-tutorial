ac0923e2724af144f221c882d1a098585dc90937
Raphaël Huc <raph82130@gmail.com>
2021-12-19 17:02:08 +0100

    Exercice 9 : Annotation des dépendances

--- dvdstore/src/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java
+++ dvdstore/src/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java
@@ -3,10 +3,12 @@ package com.mycompany.dvdstore.service;
 import com.mycompany.dvdstore.entity.Movie;
 import com.mycompany.dvdstore.repository.FileMovieRepository;
 import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 public class DefaultMovieService implements MovieServiceInterface {
 
+    @Autowired
     private MovieRepositoryInterface movieRepository;
 
     public MovieRepositoryInterface getMovieRepository() {
