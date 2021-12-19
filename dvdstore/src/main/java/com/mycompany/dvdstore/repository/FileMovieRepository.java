58b45670d833dff0a22cbb1673ea39d596a69531
Raphaël Huc <raph82130@gmail.com>
2021-12-19 16:45:37 +0100

    Exercice 7 : Valoriser une propriété de type File

--- dvdstore/src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
+++ dvdstore/src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
@@ -2,17 +2,29 @@ package com.mycompany.dvdstore.repository;
 
 import com.mycompany.dvdstore.entity.Movie;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileMovieRepository implements MovieRepositoryInterface{
 
+    private File file;
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
     public void add (Movie movie) {
         FileWriter writer;
         try{
-            writer=new FileWriter("C:\\temp\\movies.txt",true);
+            writer=new FileWriter(file,true);
             writer.write( movie.getTitle() + ";" + movie.getGenre() + "\n");
             writer.close();
+            System.out.println("Le film " + movie.getTitle() + " a été rajouté avec succès");
         }
         catch (IOException e){
             e.printStackTrace();
