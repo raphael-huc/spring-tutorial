73e5846265dedba77827c6d81fc851c3d996af4d
Raphaël Huc <raph82130@gmail.com>
2022-01-10 21:35:58 +0100

    Exercice 11 : Détection automatique des beans

--- dvdstore/src/main/java/com/mycompany/dvdstore/App.java
+++ dvdstore/src/main/java/com/mycompany/dvdstore/App.java
@@ -1,8 +1,6 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
-import com.mycompany.dvdstore.repository.FileMovieRepository;
-import com.mycompany.dvdstore.service.DefaultMovieService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
--- dvdstore/src/main/java/com/mycompany/dvdstore/repository/memory/MemoryMovieRepository.java
+++ dvdstore/src/main/java/com/mycompany/dvdstore/repository/memory/MemoryMovieRepository.java
@@ -1,11 +1,14 @@
-package com.mycompany.dvdstore.repository;
+package com.mycompany.dvdstore.repository.memory;
 
 import com.mycompany.dvdstore.entity.Movie;
+import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MemoryMovieRepository implements MovieRepositoryInterface{
+@Repository
+public class MemoryMovieRepository implements MovieRepositoryInterface {
 
     public List<Movie> movies = new ArrayList<Movie>();
 
